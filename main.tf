8b94c28fd697b17df7b4afaed0d4d3986a8dc15f
priyanshuprafful <priyanshuprafful@gmail.com>
2024-02-09 15:01:14 +0530

    updating ./tf-module-vpc

--- main.tf
+++ main.tf
@@ -10,21 +10,50 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "public_subnets" {
   vpc_id = aws_vpc.main.id # the main vpc that we created above
-  tags = merge(
-    var.tags ,
-    { Name = "${var.env}-${each.value["name"]}"}
-  )
+
   for_each = var.public_subnets
   cidr_block = each.value["cidr_block"]
   availability_zone = each.value["availability_zone"]
-}
-resource "aws_subnet" "private_subnets" {
-  vpc_id = aws_vpc.main.id # the main vpc that we created above
   tags = merge(
     var.tags ,
     { Name = "${var.env}-${each.value["name"]}"}
   )
+}
+
+##Public Route Tables
+resource "aws_route_table" "public-route-table" {
+  vpc_id = aws_vpc.main.id
+
+  for_each = var.public_subnets
+  tags = merge(
+    var.tags ,
+    { Name = "${var.env}-${each.value["name"]}-route_table"} # adding route_table at the end for better understanding
+  )
+
+}
+
+
+## Private Subnets
+resource "aws_subnet" "private_subnets" {
+  vpc_id = aws_vpc.main.id # the main vpc that we created above
+
   for_each = var.private_subnets
   cidr_block = each.value["cidr_block"]
   availability_zone = each.value["availability_zone"]
+  tags = merge(
+    var.tags ,
+    { Name = "${var.env}-${each.value["name"]}"}
+  )
+}
+
+# Private Route Tables
+resource "aws_route_table" "private-route-table" {
+  vpc_id = aws_vpc.main.id
+
+  for_each = var.private_subnets
+  tags = merge(
+    var.tags ,
+    { Name = "${var.env}-${each.value["name"]}-route_table"} # adding route_table at the end for better understanding
+  )
+
 }
