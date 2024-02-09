b0865ced3debe76a3ab358d6b6f1320f81f83a58
priyanshuprafful <priyanshuprafful@gmail.com>
2024-02-09 14:05:02 +0530

    updating ./tf-module-vpc

--- main.tf
+++ main.tf
@@ -1,4 +1,23 @@
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
-  tags = merge(var.tags , { Name = "${var.env}-vpc"} )
+  tags = merge(
+    var.tags ,
+    { Name = "${var.env}-vpc"}
+  )
+}
+
+## Public Subnets
+
+resource "aws_subnet" "public_subnets" {
+  vpc_id = aws_vpc.main.id # the main vpc that we created above
+  tags = merge(
+    var.tags ,
+    { Name = "${var.env}-${each.value["name"]}"}
+  )
+  for_each = var.public_subnets
+  cidr_block = each.value["cidr_block"]
+  availability_zone = each.value["availability_zone"]
+
+
+
 }
