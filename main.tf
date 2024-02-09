f2fdc19068ec0df26208704e2d11e83f076f33a6
priyanshuprafful <priyanshuprafful@gmail.com>
2024-02-09 18:50:35 +0530

    updating ./tf-module-vpc

--- main.tf
+++ main.tf
@@ -35,7 +35,8 @@ resource "aws_route_table" "public-route-table" {
 resource "aws_route_table_association" "public-association" {
   for_each = var.public_subnets
   subnet_id = aws_subnet.public_subnets[each.value["name"]].id
-  route_table_id = aws_route_table.public-route-table[each.value["name"]].id
+#  route_table_id = aws_route_table.public-route-table[each.value["name"]].id
+  route_table_id = lookup(lookup(aws_route_table.public-route-table, [each.value["name"]] , null ), "id" , null )
 
 }
 
