ce3fd508ef3f524cd519b623b126978b98fc0be6
priyanshuprafful <priyanshuprafful@gmail.com>
2024-02-14 17:48:20 +0530

    updating ./tf-module-vpc

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "public_route_tables" {
   value = aws_route_table.public-route-table
 }
+output "private_subnets" {
+  value = aws_subnet.private_subnets
+}
+output "public_subnets" {
+  value = aws_subnet.public_subnets
+}
